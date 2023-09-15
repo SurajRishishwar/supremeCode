dcc1da2d185986feb697dbceca96cd5d7b3d3e80
Suraj Sharma <rishishwarsuraj@gmail.com>
2023-09-15 13:03:19 +0530

    Searching Question

---
+++ Searching & Sorting/peakelement.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int peakelement(int arr[4],int size){
+    int start=0;
+    int end=size-1;
+    int ans=-1;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(arr[mid]<arr[mid+1]){
+            start=mid+1;
+        }else{
+            ans=mid+1;
+            end=mid-1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int arr[4]={0,2,1,0};
+    int size=4;
+    cout<<"Element is at "<<peakelement(arr,size)<<" position";
+
+    return 0;
+}
